e8e0ab2faf1b4ca63bf19e30f1b673c2bf8d418d
Klint Washburn <Klint@goldclaw.com>
2020-11-30 16:27:35 -0700

    Added an input that recieves an image. Next step is to save the image.

---
+++ Controllers/UploadController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace ProductPage.Controllers
+{
+    public class UploadController : Controller
+    {
+        [HttpPost("/ie")]
+        private async Task<bool> GetImage(IFormFile ImageFile)
+        {
+            if (ImageFile != null && ImageFile.Length > 0)
+            {
+                var fileName = Path.GetFileName(ImageFile.FileName);
+                var filePath = Path.Combine(Directory.GetCurrentDirectory(), @"wwwroot\img", fileName);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await ImageFile.CopyToAsync(fileStream);
+                }
+                return true;
+            }
+            return false;
+        }
+    }
+}
